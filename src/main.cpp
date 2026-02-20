01077344399c0872bd20f67f28b6930aeea228fc
Bartosz Wos <bartosz.wos.cs@gmail.com>
2026-02-20 13:11:39 +0100

    refactor: Extract Suffix Automaton logic into separate header and source files

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include "SuffixAutomaton.h"
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,76 +11,6 @@
 namespace fs = std::filesystem;
 
 constexpr char SEPARATOR = '\1';
-
-struct State{
-	int len, link;
-	std::map<char, int> next;
-	long long first_pos = -1;
-};
-
-std::vector<State> st;
-int sz, last;
-
-std::vector<std::vector<int>> inv_link;
-
-void sa_init(){
-	st.clear();
-	st.push_back({0, -1, {}, -1});
-	sz = 1;
-	last = 0;
-}
-
-void sa_extend(char c, long long current_pos){
-	int cur = sz++;
-	st.emplace_back();
-
-	st[cur].len = st[last].len + 1;
-	st[cur].first_pos = current_pos;
-	st[cur].next.clear();
-	st[cur].link = -1;
-
-	int p = last;
-	while(p != -1 && st[p].next.find(c) == st[p].next.end()){
-		st[p].next[c] = cur;
-		p = st[p].link;
-	}
-	if(p == -1){
-		st[cur].link = 0;
-	}else{
-		int q = st[p].next[c];
-		if(st[p].len + 1 == st[q].len){
-			st[cur].link = q;
-		}else{
-			int clone = sz++;
-			st.emplace_back();
-			st[clone].len = st[p].len + 1;
-			st[clone].next = st[q].next;
-			st[clone].link = st[q].link;
-			st[clone].first_pos = st[q].first_pos;
-			while(p != -1 && st[p].next[c] == q){
-				st[p].next[c] = clone;
-				p = st[p].link;
-			}
-			st[q].link = st[cur].link = clone;
-		}
-	}
-	last = cur;
-}
-
-void build_tree(){
-        inv_link.assign(sz, std::vector<int>());
-        for(int node = 1; node < sz; node++)
-                inv_link[st[node].link].push_back(node);
-}
-
-void get_occurences(int v, std::vector<long long>& positions){
-	if(st[v].first_pos != -1){
-		positions.push_back(st[v].first_pos);
-	}
-	for(const int&u : inv_link[v]){
-		get_occurences(u, positions);
-	}
-}
 
 int main(){
 	std::string root_path = ".";
